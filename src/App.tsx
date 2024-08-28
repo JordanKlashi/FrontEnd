64b0ddb1ae7fdf14a1532c71891b768633a63d3d
Verbreuk Jordan <jordan-010@hotmail.com>
2024-08-28 16:36:20 +0200

    few adjustement
    add readme and features

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,7 @@
-
-import logo from './logo.svg';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className='text-red-500'>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="text-rose-600"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-          <p className='text-rose-600'></p>
-        </a>
-      </header>
+    <div>
+      <h1 className="text-2xl text-red-400">Hello, world!</h1>
     </div>
   );
 }
